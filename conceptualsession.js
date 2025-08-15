043d369ee0a7922794392a2486edcfdb2313ed41
emon22830 <mdemon22830@gmail.com>
2025-08-15 18:02:43 +0800

    Task-1 completed

---
+++ conceptualsession.js
@@ -0,0 +1,19 @@
+
+//Task-1
+function sum(num1, num2){
+    let result = num1 + num2;
+    return (num1+num2) ;
+}
+
+function multiply(num1, num2){
+    let result = num1 * num2;
+    return (result)
+}
+
+sum(23,45);
+multiply(2,3)
+
+
+let result = sum(34,36)+sum(3,2)
+
+console.log(result);
