303e1f27a7becb3aacd5ba438bb0b462b9957331
emon22830 <mdemon22830@gmail.com>
2025-08-15 18:13:55 +0800

    Task 2 completed

--- conceptualsession.js
+++ conceptualsession.js
@@ -1,19 +1,36 @@
 
 //Task-1
-function sum(num1, num2){
-    let result = num1 + num2;
-    return (num1+num2) ;
+// function sum(num1, num2){
+//     let result = num1 + num2;
+//     return (num1+num2) ;
+// }
+
+// function multiply(num1, num2){
+//     let result = num1 * num2;
+//     return (result)
+// }
+
+// sum(23,45);
+// multiply(2,3)
+
+
+// let result = sum(34,36)+sum(3,2)
+
+// console.log(result);
+
+//Task-02
+function sumOfArray(arr){
+    if(Array.isArray(arr) == false){
+        return "Invalid"
+    }
+    let sum = 0;
+    for(let num of arr){
+        if(typeof num !=="number"){
+            return "Invalid";
+        }
+        sum = sum+num
+    }
+    return sum
 }
 
-function multiply(num1, num2){
-    let result = num1 * num2;
-    return (result)
-}
-
-sum(23,45);
-multiply(2,3)
-
-
-let result = sum(34,36)+sum(3,2)
-
-console.log(result);
+console.log(sumOfArray([1,2,3,"ekro" ,6,8,9]))
